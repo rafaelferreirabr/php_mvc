64eddb473e0b01248554e6f0d2fe44286c6c4052
Rafael Ferreira <rafamarques03@gmail.com>
2018-01-16 13:35:40 -0200

    add method render in indexController

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,15 +4,30 @@ namespace App\Controllers;
 
 Class IndexController
 {
+	private $view;
+
+	public function __construct()
+	{
+		$this->view = new \stdClass;
+	}
+
 	public function index()
 	{
-		$cars = array("Mustang","Ferrari");
-		include_once "../App/Views/index/index.phtml";
+		$this->view->cars = array("Mustang","Ferrari");
+		$this->render("index");
 	}
 
 	public function contact()
 	{
-		$cars = array("Mustang","Ferrari");
-		include_once "../App/Views/index/contact.phtml";
+		$this->view->cars = array("Mustang","Ferrari");
+		$this->render("index");
+
+	}
+
+	public function render($action)
+	{
+		$current = get_class($this);
+		$singleClassName = strtolower(str_replace("Controller", "", str_replace("App\\Controllers\\","", $current )));
+		include_once "../App/Views/".$singleClassName."/".$action.".phtml";
 	}
 }
