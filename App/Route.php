e17900c3fb478279ffb4eda0451a49a770d317f5
Rafael Ferreira <rafamarques03@gmail.com>
2018-01-16 13:06:25 -0200

    abstracting the Route Class to Vendor\SON\Init\Bootstrap

--- App/Route.php
+++ App/Route.php
@@ -1,42 +1,18 @@
 <?php
 namespace App;
 
-Class Route
+use SON\Init\Bootstrap;
+
+Class Route extends Bootstrap
 {
-	private $routes;
+	
 
-	public function __construct()
-	{
-		$this->initRoutes();
-		$this->run($this->getUrl());
-	}
-
-	public function initRoutes()
+	protected function initRoutes()
 	{
 		$routes['home'] = array('route' =>'/','controller'=>'indexController','action'=>'index');
 		$routes['contact'] = array('route' =>'/contact','controller'=>'indexController','action'=>'contact');
 		$this->setRoutes($routes);
 	}
 
-	public function setRoutes(array $routes)
-	{
-		$this->routes = $routes;
-	}
-
-	public function run($url)
-	{
-		array_walk($this->routes, function($route) use ($url){
-			if($url == $route['route']){
-				$class = "App\\Controllers\\".ucfirst($route['controller']);
-				$controller = new $class;
-				$action = $route['action'];
-				$controller->$action();
-			}
-		});
-	}
-
-	public function getUrl()
-	{
-		return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
-	}
+	
 }
