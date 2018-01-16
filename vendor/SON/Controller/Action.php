55a8ad2702d2c4eeacaecd7e9f918d5fa41143f7
Rafael Ferreira <rafamarques03@gmail.com>
2018-01-16 14:06:12 -0200

    add layout and add method content in action class

--- vendor/SON/Controller/Action.php
+++ vendor/SON/Controller/Action.php
@@ -12,9 +12,20 @@ abstract class Action
 		$this->view = new \stdClass;
 	}
 
-	protected function render($action)
+	protected function render($action,$layout = true)
 	{
 		$this->action = $action;
+
+		if ($layout == true && file_exists("../App/Views/layout.phtml")) {
+			include_once "../App/Views/layout.phtml";
+		}else{
+			
+			$this->content();
+		}
+	}
+
+	protected function content()
+	{
 		$current = get_class($this);
 		$singleClassName = strtolower(str_replace("Controller", "", str_replace("App\\Controllers\\","", $current )));
 		include_once "../App/Views/".$singleClassName."/".$this->action.".phtml";
